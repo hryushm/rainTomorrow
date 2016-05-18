eff1ac9ae7ab880983d547f0014ec87c40ef8b38
kuroshichimi <kuroshichimi03@gmail.com>
2016-05-19 00:33:23 +0900

    class化

--- main.rb
+++ main.rb
@@ -1,46 +1,52 @@
 require 'json'
 require 'open-uri'
 
-BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
-API_KEY = File.open(File.dirname(__FILE__) + "/apikey").read
+class Weather
 
-def isTomorrow(unix_time)
-    day = Time.at(unix_time).day
-    Time.now.day + 1 == day
-end
+    BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
+    API_KEY = File.open(File.dirname(__FILE__) + "/apikey").read
 
-def is6or9am(unix_time)
-    time = Time.at(unix_time).hour
-    time == 6 || time == 9
-end
+    def initialize(city)
+        response = open(BASE_URL + "?q=#{city}&units=metric&APPID=#{API_KEY}")
+        @data = JSON.parse(response.read)
+    end
 
-def getWeather(city)
-    response = open(BASE_URL + "?q=#{city}&units=metric&APPID=#{API_KEY}")
-    JSON.parse(response.read)
-end
+    def show
+        JSON.pretty_generate(@data)
+    end
 
-def rainTomorrow?(city)
-    res = getWeather(city)
-    res['list'].each do |li|
-        if isTomorrow(li['dt']) && is6or9am(li['dt'])
-            min = li['main']['temp_min']
-            max = li['main']['temp_max']
-            weather = li['weather'][0]['main']
-            if weather == 'Rain'
-                return true
+    def rainTomorrow?
+        @data['list'].each do |li|
+            if isTomorrow(li['dt']) && is6or9am(li['dt'])
+                min = li['main']['temp_min']
+                max = li['main']['temp_max']
+                weather = li['weather'][0]['main']
+                if weather == 'Rain'
+                    return true
+                end
             end
         end
+        return false
     end
-    return false
-end
 
-def getWeather69(city)
-    res = getWeather(city)
-    arr = Array.new
-    res['list'].each do |li|
-        if isTomorrow(li['dt']) && is6or9am(li['dt'])
-            arr.push(li['weather'][0]['description'])
+    def getWeather69
+        arr = Array.new
+        @data['list'].each do |li|
+            p li['dt']
+            if isTomorrow(li['dt']) && is6or9am(li['dt'])
+                arr.push(li['weather'][0]['description'])
+            end
         end
+        return arr
     end
-    return arr
+
+    private
+    def isTomorrow(unix_time)
+        day = Time.at(unix_time).day
+        Time.now.day + 1 == day
+    end
+    def is6or9am(unix_time)
+        p time = Time.at(unix_time).hour
+        time == 6 || time == 9
+    end
 end
